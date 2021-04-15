cf0c7cdc3a8de18cebe55ba1f6c9beb5fe0ae13e
Adam Maracz <adam.maracz@me.com>
2021-04-15 12:18:37 +0200

    Update metube-webos-config.php
    removed metube specific lines as the new wrapper does not use it anymore

--- metube-webos-config.php
+++ metube-webos-config.php
@@ -2,8 +2,6 @@
 return array(
 	'api_key' => '<YOUTUBE API KEY>', //See https://console.developers.google.com/apis/api/youtube.googleapis.com/
 	'file_dir' => '/downloads/',
-	'metube_url' => 'http://metube_webos_service_1:8081',
-	'metube_port' => 8081,
 	'client_key' => 'webos2009060606',	//If you want some security, replace this with a random string here and in debug_key. Your client must know this value.
 	'debug_key' => 'webos2009060606',	//If you want some security, replace this with a random string here and in client_key. Your client must know this value.
 	'server_id' => 'webos2009060606'	//If you want some more security, replace this with a random string here. Your client must know this value.
